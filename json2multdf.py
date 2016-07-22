fca0fad6cde972dc15ad248aabf4bc620eaeeaaa
Philipp Dufter <philipp.dufter@web.de>
2016-07-22 10:51:17 +0100

    add parent key storage lists

--- json2multdf.py
+++ json2multdf.py
@@ -6,17 +6,24 @@ def decrease_levels(nested_dict):
     
     list_of_levels = [[nested_dict.items()]]
 
+    previous_parent_keys = [tuple()]
+
     for level in list_of_levels:
         new_level = list()
+        parent_keys = list()
+        subdict_counter = 0
+
         for subdict in level:
             for key, value in subdict:
                 if isinstance(value, dict):
+                    parent_keys.append(previous_parent_keys[subdict_counter] + (key,))
                     new_level.append(value.items())
                 else:
                     print key
-        
+            subdict_counter = subdict_counter + 1
         if new_level != list():
             list_of_levels.append(new_level)
+        previous_parent_keys = parent_keys
     return None
 
 
