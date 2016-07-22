ff4f164ac8e1b4363f1ef5cce40623675d908dee
Philipp Dufter <philipp.dufter@web.de>
2016-07-22 11:09:06 +0100

    dataframe conversion

--- json2multdf.py
+++ json2multdf.py
@@ -2,18 +2,15 @@ import json
 import pandas
 
 
-def decrease_levels(nested_dict):
+def decrease_dict_levels(nested_dict):
+    # returns flattened dict with tuples as keys
     resulting_dict = {}
-    
     list_of_levels = [[nested_dict.items()]]
-
     previous_parent_keys = [tuple()]
-
     for level in list_of_levels:
         new_level = list()
         parent_keys = list()
         subdict_counter = 0
-
         for subdict in level:
             for key, value in subdict:
                 if isinstance(value, dict):
@@ -28,12 +25,18 @@ def decrease_levels(nested_dict):
     return resulting_dict
 
 
+def convert_json2multidf(path_to_json, create_multiindex=True):
+    with open(path_to_json, 'r') as json_data:
+        result = pandas.DataFrame(decrease_dict_levels(json.loads(line)) for line in  json_data)
+    return result
+
+
 if __name__ == '__main__':
     
     json_file = 'data/fake_data.json'
 
     with open(json_file, 'r') as d:
         for line in d:
-            result = decrease_levels(json.loads(line))
+            result = decrease_dict_levels(json.loads(line))
 
-    print result
+    print convert_json2multidf(json_file)
